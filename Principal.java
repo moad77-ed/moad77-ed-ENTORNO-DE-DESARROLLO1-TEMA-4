a38629628c0d72556058862b5430bc3c867a3357
Moad <8811464@alu.murciaeduce.es>
2026-02-05 12:19:34 +0000

    Clase Usuario y arrays

--- Principal.java
+++ Principal.java
@@ -1,12 +1,48 @@
-import java.time.LocalDateTime;
-public class Principal{
-    public static void(String[] args){
-        System.out.print("Hola mundo");
-        mostrarHora();
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el número de usuarios: ");
+        int numUsuarios = sc.nextInt();
+        sc.nextLine(); // limpiar buffer
+
+        Usuario[] usuarios = crearArrayUsuarios(numUsuarios);
+
+        System.out.println("\nListado de usuarios:");
+        for (Usuario u : usuarios) {
+            System.out.println("Nombre: " + u.nombre);
+            System.out.println("Apellidos: " + u.apellidos);
+            System.out.println("Email: " + u.email);
+
+        }
+
     }
-    public static void mostrarHora(){
-    LocalDateTime ahora = LocalDateTime.now();
-    System.out.print("Fecha:" + ahora);
-    
+
+    public static Usuario[] crearArrayUsuarios(int numero) {
+
+        Scanner sc = new Scanner(System.in);
+        Usuario[] usuarios = new Usuario[numero];
+
+        for (int i = 0; i < numero; i++) {
+            System.out.println("\nUsuario " + (i + 1));
+
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Apellidos: ");
+            String apellidos = sc.nextLine();
+
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+
+            usuarios[i] = new Usuario(nombre, apellidos, email);
+        }
+
+        return usuarios;
     }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Se ha añadido una modificacion
+## Práctica 4.1B
+
+
+- Una clase `Usuario` con los atributos públicos:
+  - nombre
+  - apellidos
+  - email
+- Un método que recibe un número y crea un array de objetos `Usuario` de ese tamaño.
+- El programa solicita por teclado los datos de cada usuario.
+- Se muestra por pantalla la información de todos los usuarios creados.
+
